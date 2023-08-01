af0d5b0d4c5cd704604f9fcf8637bb344fe1dd50
Andrey <AlpatoV_A_A@mail.ru>
2023-08-01 22:06:41 +0300

    2.1. FileInputStream

---
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+import static java.awt.SystemColor.text;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("data/input.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            System.out.println(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String[] lines = text.toString().split(System.lineSeparator());
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}

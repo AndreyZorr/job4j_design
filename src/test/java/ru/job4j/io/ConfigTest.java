945a7da516aedc087967d70661eb077dfc2168f1
Andrey <AlpatoV_A_A@mail.ru>
2023-08-06 19:30:56 +0300

    6. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair without comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenThereAreMoreEquals1() {
+        String path = "./data/addit.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Andrey=");
+    }
+    @Test
+    void whenSomeLinesAreEmpty() {
+        String path = "./data/empty.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("age")).isEqualTo("32");
+    }
+    @Test
+    void whenBrokenTemplate() {
+        String path = "./data/brokentemp.properties";
+        Config config = new Config(path);
+       IllegalArgumentException exception = assertThrows(
+               IllegalArgumentException.class,
+               config::load
+       );
+       assertThat(exception.getMessage()).isEqualTo("=");
+    }
+}

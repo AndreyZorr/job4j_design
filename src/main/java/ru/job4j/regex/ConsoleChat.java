28f226e22db3e69ef3e5afc930815ed2bef723ec
Andrey <AlpatoV_A_A@mail.ru>
2023-08-25 22:25:57 +0300

    16. Кодировка [#862]

---
+++ src/main/java/ru/job4j/regex/ConsoleChat.java
@@ -0,0 +1,78 @@
+package ru.job4j.regex;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ConsoleChat {
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+    private final String path;
+    private final String botAnswers;
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+        boolean rsl = true;
+        List<String> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Здраствуйте !");
+        String name = input.nextLine();
+        List<String> readPhrases = readPhrases();
+        while (!name.equals(OUT)) {
+            list.add(name);
+            if (name.equals(STOP)) {
+                rsl = false;
+            } else if (name.equals(CONTINUE)) {
+                rsl = true;
+            } else if (rsl) {
+                String answer = getPhrasese(readPhrases);
+                System.out.println(answer);
+                list.add(answer);
+            }
+            name = input.nextLine();
+        }
+        list.add(name);
+        saveLog(list);
+    }
+
+    private String getPhrasese(List<String> phrases) {
+        return phrases.get(new Random().nextInt(phrases.size()));
+    }
+
+    private List<String> readPhrases() {
+        List<String> strings = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(botAnswers))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                strings.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return strings;
+    }
+
+    private void saveLog(List<String> log) {
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(path))) {
+            for (String line : log) {
+                printWriter.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("data/path.txt",
+                "data/botAnswers.txt");
+        cc.run();
+    }
+}

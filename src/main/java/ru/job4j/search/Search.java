9f280d612d6d2a70f8a004d192adee7af53c23a8
Andrey <AlpatoV_A_A@mail.ru>
2023-09-18 10:17:46 +0300

    3. Поиск файлов по критерию [#783 #445686]

--- src/main/java/ru/job4j/search/Search.java
+++ src/main/java/ru/job4j/search/Search.java
@@ -1,19 +1,20 @@
 package ru.job4j.search;
 
-import ru.job4j.io.ArgsName;
 import ru.job4j.io.SearchFiles;
-
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
-
 public abstract class Search {
+
     public static void main(String[] args) {
         if (args.length != 4) {
-            throw new IllegalArgumentException("arguments must be 4");
+            throw new IllegalArgumentException("Arguments must be 4");
         }
         ArgsName argsName = ArgsName.of(args);
         validate(argsName);
@@ -26,11 +27,14 @@ public abstract class Search {
                 new BufferedOutputStream(
                         new FileOutputStream(argsName.get("o"))
                 ))) {
-            if ("mask".equals(argsName.get("t"))) {
+            if ("name".equals(argsName.get("t"))) {
+                search(path, p -> p.toFile().getName().equals(argsName.get("n"))).forEach(out::println);
+            } else if ("mask".equals(argsName.get("t"))) {
+                Filter filter = new Filter(preparePattern(argsName.get("n")));
+                search(path, p -> filter.accept(path.toFile(), p.toFile().getName())).forEach(out::println);
+            } else {
                 Pattern pattern = Pattern.compile(argsName.get("n"));
                 search(path, p -> pattern.matcher(p.toFile().getName()).matches()).forEach(out::println);
-            } else {
-                search(path, p -> p.toFile().getName().equals(argsName.get("n"))).forEach(out::println);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -61,5 +65,11 @@ public abstract class Search {
         if (args.get("o").length() < 1) {
             throw new IllegalArgumentException("Wrong path");
         }
+    }
+
+    private static String preparePattern(String pattern) {
+        String value = pattern.replace(".", "\\.");
+        String value2 = value.replace("?", ".?");
+        return value2.replace("*", ".*");
     }
 }

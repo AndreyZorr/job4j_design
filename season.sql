854e1ea80d1be95decca3f7b78d81d438fdb08ff
ANDREYALPATO <110410351+AndreyZorr@users.noreply.github.com>
2023-09-19 13:48:14 +0300

    Add files via upload

--- season.sql
+++ season.sql
@@ -1,1 +1,9 @@
+create table season(
+id serial primary key,
+name varchar(255),
+temperature date,	
+precipitation text
+);
+insert into season(name, temperature, precipitation) values('winter', '-20.4', 'snow');
+update season set temperature = '-30';
 delete from season;

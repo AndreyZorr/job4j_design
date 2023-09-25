7740ec7dc5baca82c2f8c9f4f67da622019f65dd
ANDREYALPATO <110410351+AndreyZorr@users.noreply.github.com>
2023-09-25 23:09:19 +0300

    1. Inner join [#373617]

---
+++ src/main/java/db/Inner join.sql
@@ -0,0 +1,21 @@
+create table human(
+	id serial primary key,
+	name varchar(255)
+);
+
+create table tv(
+	id serial primary key,
+	name text,
+	size int,
+	human_id int references human(id)
+);
+
+insert into human(name) values('Roma');
+
+insert into tv(name, size, human_id) values ('rsc', 32, 1);
+insert into tv(name, size, human_id) values ('rrr', 48, 1);
+insert into tv(name, size, human_id) values ('sms', 108, 1);
+insert into tv(name, size, human_id) values ('tlc', 32, 1);
+
+select h.name as "Имя владельца", t.name "Название телевизора", t.size as "Размер"
+from tv t join human h on t.human_id = h.id;

daacefaf245541554b0f9146aba1f78b699619e9
ANDREYALPATO <110410351+AndreyZorr@users.noreply.github.com>
2023-10-12 10:07:21 +0300

    3. Хранимые процедуры и функции [#504804]

---
+++ src/main/java/db/procedure_function.sql
@@ -0,0 +1,33 @@
+create table products (
+    id serial primary key,
+    name varchar(50),
+    producer varchar(50),
+    count integer default 0,
+    price integer
+);
+
+create or replace procedure delete_data(d_count integer, d_id integer)
+language 'plpgsql'
+as $$
+    BEGIN
+	if d_count <= 0 THEN
+    delete from products;
+    end if;
+	END
+$$;
+
+create or replace function f_delete_data(d_count integer, d_id integer)
+returns integer
+language 'plpgsql'
+as
+$$
+    declare
+        result integer;
+    begin
+        if d_count <= 0 THEN
+            delete from products;
+			select delete count from products where id = d_id;
+    end if;
+    return result;
+	end;
+$$;
